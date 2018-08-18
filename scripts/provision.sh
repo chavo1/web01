dfbace27bbe0036801b3d4773aebf7fd3730053a
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-18 06:11:49 +0300

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# make apt to install just packages
+mkdir -p /etc/dpkg/dpkg.cfg.d
+cat >/etc/dpkg/dpkg.cfg.d/01_nodoc <<EOF
+path-exclude /usr/share/doc/*
+path-include /usr/share/doc/*/copyright
+path-exclude /usr/share/man/*
+path-exclude /usr/share/groff/*
+path-exclude /usr/share/info/*
+path-exclude /usr/share/lintian/*
+path-exclude /usr/share/linda/*
+EOF
+
 # make apt non-interactive and less verbose
 export DEBIAN_FRONTEND=noninteractive
 export APTARGS="-qq -o=Dpkg::Use-Pty=0"
@@ -13,6 +25,18 @@ which curl nginx || {
 
 # stop nginx service
 service nginx stop
+
+# remove default conf of nginx
+[ -f /etc/nginx/sites-available/default ] && {
+  rm -fr /etc/nginx/sites-available/default
+}
+
+# copy our nginx conf
+cp nginx.conf /etc/nginx/sites-available/default
+
+# deploy our website
+mkdir -p /var/www/html/
+cp index.html /var/www/html/index.html
 
 # start nginx service
 service nginx start
